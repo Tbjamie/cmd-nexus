5061ae3859f68a10b63160e65d8f7e00f4ae401b
Tbjamie <144114217+Tbjamie@users.noreply.github.com>
2025-05-15 00:04:14 +0200

    feat: finished IconButton compoonent

--- src/lib/components/buttons/IconButton.svelte
+++ src/lib/components/buttons/IconButton.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
-	export let element: 'button' | 'a' = 'button';
+	export let href = '';
+	export let target: '_self' | '_blank' = '_self';
+	export let type: 'button' | 'submit' | 'reset' = 'button';
 </script>
 
-<svelte:element this={element} class="icon-button">
+<svelte:element
+	this={href ? 'a' : 'button'}
+	target={href ? target : null}
+	type={href ? null : type}
+	href={href ? href : null}
+	class="icon-button"
+>
 	<span>
 		<slot />
 	</span>
@@ -15,7 +23,37 @@
 		padding: 0.5rem;
 		border: none;
 		position: relative;
+		text-decoration: none;
 		mix-blend-mode: color-dodge;
 		aspect-ratio: 1 / 1;
+		cursor: pointer;
+		transition: 200ms;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		color: initial;
+		position: relative;
+	}
+
+	.icon-button::after {
+		content: '';
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		left: 0;
+		top: 0;
+		box-shadow: 0 0 16px var(--black);
+		border-radius: 9999px;
+	}
+
+	.icon-button:hover,
+	.icon-button:focus-visible {
+		--opacity: 80%;
+	}
+
+	.icon-button:active {
+		--opacity: 100%;
+		background: var(--purple-light);
+		transition: 100ms;
 	}
 </style>
